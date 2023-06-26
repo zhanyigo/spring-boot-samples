58d084043e6b2e89e8c0d66d8c0cdbd80777f906
zhanyi <zyzy257@gmail.com>
2023-06-26 10:25:27 +0800

    init

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(true);
 	}
 
 }
